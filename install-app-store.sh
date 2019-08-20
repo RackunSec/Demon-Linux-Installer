e9e02765c534adb476bd6067e8813c62e4b212ae
WeakNetLabs <weaknetlabs@gmail.com>
2019-08-20 20:58:58 +0000

    Migrated all Demon App Store stuff.

--- install-app-store.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp demon-app-store.sh /usr/local/sbin/
-chmod +x /usr/local/sbin/demon-app-store.sh
-mkdir -p /usr/share/demon/images/icons 2>/dev/null
-cp icons/* /usr/share/demon/images/icons
