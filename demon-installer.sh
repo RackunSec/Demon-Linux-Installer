bb975a8a47b27f9d196733427e8c20851e18491a
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-08-22 14:54:23 -0400

    Migrated UpdateMe to init script

--- demon-installer.sh
+++ demon-installer.sh
@@ -57,27 +57,6 @@ quit (){
   exit 1
 }
 
-### Update the installer EACH TIME RAN
-updateMe () {
- if [ ! -d /appdev/Demon-Linux-Installer ]
-  then
-   mkdir -p /appdev/
-   cd /appdev
-   git clone https://github.com/weaknetlabs/Demon-Linux-Installer/
-   rm /usr/local/sbin/demon-installer.sh # get rid of old version
- else
-  cd /appdev/Demon-Linux-Installer
-  git pull
- fi # does the icons directory exist?
- if [ ! -d /usr/share/demon/images/icons ]
-  then
-   mkdir -p /usr/share/demon/images/icons
- fi
- cp /appdev/Demon-Linux-Installer/demon-installer.sh /usr/local/sbin/demon-installer.sh
- cp /appdev/Demon-Linux-Installer/icons/* /usr/share/demon/images/icons/
- printf "[!] Updated to the latest version. \n"
-}
-
 ### Ensure insternet connection
 progressBar "Creating an active internet connection.\n" &
 /etc/init.d/network-manager start
@@ -92,7 +71,6 @@ killBar
 
 ### Get the latest version of me :) self-maintaining
 progressBar "Updating this installer. Please wait ... " &
-updateMe
 killBar
 
 ### Update YAD
