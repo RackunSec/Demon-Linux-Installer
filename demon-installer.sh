165ac167eafb689b773fd60b5dc1eb2dd05e35fc
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-08-30 08:33:51 -0400

    Updated for new staging area, /var/demon/

--- demon-installer.sh
+++ demon-installer.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
+### Updated for new staging area /var/demon/ ::muscle emoji goes here::
+### Douglas Berdeaux, Demon Linux, WeakNet Laboratories, 2019
 ### Update the installer EACH TIME RAN
-updateMe () {
- if [ ! -d /appdev/Demon-Linux-Installer ]
+DLI_ROOT=/var/demon/installer
+GITNAME=Demon-Linux-Installer
+GITURL=https://github.com/weaknetlabs/$GITNAME
+if [ ! -d $DLI_ROOT ]
   then
-   mkdir -p /appdev/
-   cd /appdev
-   git clone https://github.com/weaknetlabs/Demon-Linux-Installer/
-   rm /usr/local/sbin/demon-installer.sh # get rid of old version
- else
-  cd /appdev/Demon-Linux-Installer
-  #git pull
- fi # does the icons directory exist?
+    mkdir -p /var/demon/installer
+fi
+
+updateMe () {
+  if [ ! -d $DLI_ROOT/Demon-Linux-Installer ] # it exists, pull latest
+    then
+      mkdir $DLI_ROOT/$GITNAME
+  fi
+  cd $DLI_ROOT/$GITNAME
+  git $GITURL
+
  if [ ! -d /usr/share/demon/images/icons ]
   then
    mkdir -p /usr/share/demon/images/icons
  fi
  # copy the new init file:
- cp /appdev/Demon-Linux-Installer/demon-installer.sh /usr/local/sbin/demon-installer.sh
+ cp ${DLI_ROOT}/${GITNAME}/demon-installer.sh /usr/local/sbin/demon-installer.sh
  chmod +x /usr/local/sbin/demon-installer.sh
+
  # copy the new workflow file:
  cp /appdev/Demon-Linux-Installer/demon-installer-workflow.sh /usr/local/sbin/demon-installer-workflow.sh
  chmod +x /usr/local/sbin/demon-installer-workflow.sh
+
  # copy the icon images:
  cp /appdev/Demon-Linux-Installer/icons/* /usr/share/demon/images/icons/
+
  # complete.
  printf "[!] Updated to the latest version. \n"
 }
+
 # Step 1: UPDATE
 updateMe
 # Step 2: Run the Workflow portion
